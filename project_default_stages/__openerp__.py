b60caf8d00b87ab2c1912c3dcbd77793cad76b4e
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-06-28 22:25:49 +0200

    Initial commit

---
+++ project_default_stages/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Project default stages',
+    'version': '1.0',
+    'summary': 'Lets you to choose project stages',
+    'author': 'B++',
+    'website': 'http://www.bplus.plus',
+    'category': 'Projects',
+    'images': [],
+    'depends': [
+        'project'
+    ],
+    'data': [
+        'views/project_view.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    #'application': False,
+    #'auto_install': False,
+}
