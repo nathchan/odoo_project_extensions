1f336d8261a5d5ed9b1dd536c242cd6b467510c8
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-06-30 17:07:35 +0200

    New module to show task priority on tree view

---
+++ project_show_priority_on_task_tree/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Priority on task tree view',
+    'version': '1.0',
+    'summary': 'Shows priority on task tree view',
+    'author': 'B++',
+    'website': 'http://www.bplus.plus',
+    'category': 'Projects',
+    'images': [],
+    'depends': [
+        'project'
+    ],
+    'data': [
+        'views/task_view.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    #'application': False,
+    #'auto_install': False,
+}
