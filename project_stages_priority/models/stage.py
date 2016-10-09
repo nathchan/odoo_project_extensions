e64e9454f8e76389403307ec8aa4f20ba9fd60da
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-10-09 15:59:57 +0200

    project_stages_priority: column rename

--- project_stages_priority/models/stage.py
+++ project_stages_priority/models/stage.py
@@ -6,4 +6,4 @@ from openerp import models, fields, api
 class ProjectStage(models.Model):
     _inherit = 'project.task.type'
 
-    is_priority_one = fields.Boolean('Is priority one')
+    quick_view = fields.Boolean('Quick view')
