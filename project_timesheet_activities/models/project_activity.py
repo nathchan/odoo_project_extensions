4de524b6d56ccb0837e8fbc8aef7a5fcb1421ddc
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-10-09 20:36:51 +0200

    project_timesheet_activities: bool field default is false

--- project_timesheet_activities/models/project_activity.py
+++ project_timesheet_activities/models/project_activity.py
@@ -8,7 +8,7 @@ class ProjectActivityCategory(models.Model):
     name = fields.Char('Name', required=True)
     code = fields.Char('Code', requiered=True)
     info = fields.Text('Description')
-    useful = fields.Boolean('Useful')
+    useful = fields.Boolean('Useful', defult=False)
 
 class ProjectActivity(models.Model):
     _name = 'project.activity'
