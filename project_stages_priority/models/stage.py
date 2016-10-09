4ea96d23804443f6349caee948aa5b0775611074
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-10-09 20:36:25 +0200

    project_stages_priority: bool field default is false

--- project_stages_priority/models/stage.py
+++ project_stages_priority/models/stage.py
@@ -6,4 +6,4 @@ from openerp import models, fields, api
 class ProjectStage(models.Model):
     _inherit = 'project.task.type'
 
-    quick_view = fields.Boolean('Quick view')
+    quick_view = fields.Boolean('Quick view', default=False)
