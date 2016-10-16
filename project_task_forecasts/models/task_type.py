b3ddd6b0c969e70006cff6fb2d038efe6a06cc8b
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-10-16 15:15:15 +0200

    project_task_forecasts: Stages ordering by sequence

--- project_task_forecasts/models/task_type.py
+++ project_task_forecasts/models/task_type.py
@@ -5,6 +5,7 @@ from openerp import models, fields
 
 class ProjectTaskType(models.Model):
     _inherit = 'project.task.type'
+    _order = 'sequence'
 
     duration_forecast = fields.Integer('Duration forecast')
     process_ids = fields.One2many('project.task.stage.process', 'stage_id', 'Child Processes')
