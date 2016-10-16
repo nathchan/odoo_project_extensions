804b7939ddcf4fab31f67ce79147c739a112681d
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-10-16 19:54:58 +0200

    template_module: Module as template for new ones

---
+++ template_module/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Template module',
+    'version': '1.0',
+    'summary': 'Template module',
+    'author': 'B++',
+    'website': 'http://www.bplus.plus',
+    'category': 'Projects',
+    'images': [],
+    'depends': [
+        'project',
+    ],
+    'data': [
+
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    #'application': False,
+    #'auto_install': False,
+}
