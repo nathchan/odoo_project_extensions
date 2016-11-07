6a2804b37a2aa16b0b3635f10148274da6d8297c
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-11-07 19:18:41 +0100

    project_task_issues: small bug fix

--- project_task_issues/models/task.py
+++ project_task_issues/models/task.py
@@ -23,4 +23,5 @@ class ProjectTask(models.Model):
         res['context'] = context
         res['context'].update({'default_task_id': ids[0], 'default_project_id': obj.project_id.id})
         res['domain'] = [('task_id', '=', ids[0])]
+        del res['context']['group_by']
         return res
