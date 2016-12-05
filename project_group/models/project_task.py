90cd49bdf420431737297b11367c2d3cedfde454
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-12-05 23:14:35 +0100

    project_group: rename Task group into Rollout group

--- project_group/models/project_task.py
+++ project_group/models/project_task.py
@@ -6,4 +6,4 @@ from openerp import models, fields, api
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    task_group_id = fields.Many2one('project.task.group', 'Task group')
+    task_group_id = fields.Many2one('project.task.group', 'Rollout group')
