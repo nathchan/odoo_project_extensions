4517e9f4178c688210a26c33d0388bd83ef36e2b
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-12-18 12:23:00 +0100

    project_task_activities: domain on timesheet field of activity to choose only specific accounts

--- project_timesheet_activities/models/account_analytic_account.py
+++ project_timesheet_activities/models/account_analytic_account.py
@@ -11,3 +11,5 @@ class AccountAnalyticAccount(models.Model):
     category = fields.Selection([('on_project', 'On project'),
                                  ('not_on_project', 'Not on project'),
                                  ('not_in_production', 'Not in production')], 'Category')
+
+    choose_on_timesheets = fields.Boolean('Display as an option in timesheets', )
