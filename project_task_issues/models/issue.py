a00944c57642bb8d21a9f97baabd60696c75ece3
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-12-19 22:48:51 +0100

    project_task_issues: on Issues subcategory required for Missing Goods cateegory

--- project_task_issues/models/issue.py
+++ project_task_issues/models/issue.py
@@ -6,7 +6,13 @@ from openerp import models, fields, api
 class ProjectIssue(models.Model):
     _inherit = 'project.issue'
 
+    @api.depends('category_id.name')
+    def _compute_category_name(self):
+        if self.category_id:
+            self.category_name = self.category_id.name
+
     category_id = fields.Many2one('project.issue.category', 'Category')
+    category_name = fields.Char('Category Name', compute=_compute_category_name)
     subcategory_id = fields.Many2one('project.issue.subcategory', 'Subcategory', context="{'default_category_id': category_id}", domain="[('category_id','=',category_id)]")
     solution = fields.Text('Solution')
 
