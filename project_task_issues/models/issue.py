da5cac01ac5121c311cc8f8e2cfb117fad5d87e3
Nemanja Dragović <nemanjadragovic94@gmail.com>
2016-12-20 18:09:29 +0100

    project_task_issues: new column issue status to display nice string on issue tree view

--- project_task_issues/models/issue.py
+++ project_task_issues/models/issue.py
@@ -11,10 +11,20 @@ class ProjectIssue(models.Model):
         if self.category_id:
             self.category_name = self.category_id.name
 
+    @api.depends('active')
+    @api.multi
+    def _compute_status(self):
+        for rec in self:
+            if rec.active is True:
+                rec.status = 'opened'
+            else:
+                rec.status = 'closed'
+
     category_id = fields.Many2one('project.issue.category', 'Category')
     category_name = fields.Char('Category Name', compute=_compute_category_name)
     subcategory_id = fields.Many2one('project.issue.subcategory', 'Subcategory', context="{'default_category_id': category_id}", domain="[('category_id','=',category_id)]")
     solution = fields.Text('Solution')
+    status = fields.Selection([('opened', 'Opened'), ('closed', 'Closed')], 'Status', compute=_compute_status, store=True)
 
     @api.onchange('subcategory_id')
     def change_subcategory(self):
