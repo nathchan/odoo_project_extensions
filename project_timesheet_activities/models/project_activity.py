202e4a16bbd4a70dfca1707eb67fce95eae94957
Nemanja Dragović <nemanjadragovic94@gmail.com>
2017-01-13 02:47:43 +0100

    project_timesheet_activities: activities domain for General field of activity

--- project_timesheet_activities/models/project_activity.py
+++ project_timesheet_activities/models/project_activity.py
@@ -6,11 +6,30 @@ from openerp import models, fields, api
 class ProjectActivity(models.Model):
     _name = 'project.activity'
 
+    @api.multi
+    def _compute_filter_activities(self):
+        for rec in self:
+            rec.filter_activities = '---'
+
+    def _search_filter_activities(self, operator, value):
+        res_ids = []
+        if operator == '=' and value == 'General':
+            operator = 'in'
+            res_ids = [item.id for item in self.search([('is_general_activity', '=', True)])]
+        else:
+            operator = 'in'
+            res_ids = [item.id for item in self.search([])]
+        return [('id', operator, res_ids)]
+
     name = fields.Char('Name', required=True)
     info = fields.Text('Description')
     category = fields.Selection([('effective', 'Effective'), ('ineffective', 'Ineffective')], 'Category')
     on_site_activity = fields.Boolean('On site activity')
     show_on_sap_report = fields.Boolean('Show on SAP report')
+    is_general_activity = fields.Boolean('Is general activity')
+    filter_activities = fields.Char(compute=_compute_filter_activities, search=_search_filter_activities)
+
+
 
 
 
