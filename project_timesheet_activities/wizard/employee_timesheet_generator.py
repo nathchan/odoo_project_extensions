a23278724a44b200693a6757d29f6993e35a8851
Nemanja Dragović <nemanjadragovic94@gmail.com>
2017-02-25 13:12:44 +0100

    project_timesheet_activities: SAP report updated

--- project_timesheet_activities/wizard/employee_timesheet_generator.py
+++ project_timesheet_activities/wizard/employee_timesheet_generator.py
@@ -677,7 +677,7 @@ class EmployeeTimesheetGenerator(models.TransientModel):
                 ws['M'+str(n)] = line.project_activity_id.name if line.project_activity_id else ''
                 ws['N'+str(n)] = line.user_id.name
                 ws['O'+str(n)] = line.timesheet_comment if line.timesheet_comment else ''
-                ws['P'+str(n)] = d.datetime.strptime(line.create_date, tools.DEFAULT_SERVER_DATE_FORMAT)
+                ws['P'+str(n)] = d.datetime.strptime(line.create_date, tools.DEFAULT_SERVER_DATETIME_FORMAT)
                 ws['P'+str(n)].style = Style(alignment=Alignment(wrap_text=True, horizontal='center', vertical='center'),
                                              number_format="DD.MM.YYYY")
 
@@ -749,7 +749,7 @@ class EmployeeTimesheetGenerator(models.TransientModel):
                 ws['O'+str(n)] = line.timesheet_comment if line.timesheet_comment else ''
                 ws['O'+str(n)].font = Font(color=Color('e80000'))
 
-                ws['P'+str(n)] = d.datetime.strptime(line.create_date, tools.DEFAULT_SERVER_DATE_FORMAT)
+                ws['P'+str(n)] = d.datetime.strptime(line.create_date, tools.DEFAULT_SERVER_DATETIME_FORMAT)
                 ws['P'+str(n)].style = Style(font=Font(color=Color('e80000')),
                                              alignment=Alignment(wrap_text=True, horizontal='center', vertical='center'),
                                              number_format="DD.MM.YYYY")
