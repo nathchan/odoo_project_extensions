bdbbb953ecbcf1c0d0175a8074a46944c0b921d9
Nemanja Dragović <nemanjadragovic94@gmail.com>
2017-03-17 00:42:21 +0100

    project_task_forecasts: milestone_template_id column on issue tree view

--- project_task_forecasts/models/issue.py
+++ project_task_forecasts/models/issue.py
@@ -6,4 +6,13 @@ from openerp import models, fields, api
 class ProjectIssue(models.Model):
     _inherit = 'project.issue'
 
+    @api.multi
+    @api.depends('milestone_id')
+    def _compute_template_milestone(self):
+        for rec in self:
+            rec.template_milestone_id = False
+            if rec.milestone_id:
+                rec.template_milestone_id = rec.milestone_id.milestone_id
+
     milestone_id = fields.Many2one('project.task.milestone.forecast', 'Milestone', domain="[('task_id', '=', task_id)]")
+    template_milestone_id = fields.Many2one('project.milestone', 'Milestone', compute=_compute_template_milestone, store=True)
