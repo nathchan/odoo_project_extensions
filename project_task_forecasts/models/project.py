0952e4f87c909252b88b5519718a06ed99da9049
Nemanja Dragović <nemanjadragovic94@gmail.com>
2017-05-04 23:48:57 +0200

    project_task_forecast: updating milestone FC dates from project form view

--- project_task_forecasts/models/project.py
+++ project_task_forecasts/models/project.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp import models, fields, api, exceptions as ex
 
 
 class ProjectProject(models.Model):
@@ -9,3 +9,26 @@ class ProjectProject(models.Model):
     milestone_ids = fields.One2many('project.milestone', 'project_id', 'Milestones')
     project_code = fields.Char('Project ID')
     project_wp_code = fields.Char('DWP ID')
+
+    @api.one
+    def apply_milestones_template(self):
+        sql = """
+            select distinct
+                task_id
+            from
+                project_task_milestone_forecast
+            where
+                project_id = %s
+        """
+        self.env.cr.execute(sql % (self.id, ))
+        res_ids = self.env.cr.fetchall()
+        err_msgs = []
+        for task in self.env['project.task'].browse([item[0] for item in res_ids]):
+            ms = task.milestone_ids.sorted(lambda x: x.sequence_order)[0]
+            try:
+                ms.calculate_forecast()
+            except Exception as e:
+                err_msgs.append(e.name)
+
+        if len(err_msgs) > 0:
+            raise ex.UserError('\n\n\n'.join(err_msgs))
