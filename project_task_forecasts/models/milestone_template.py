e33ac71501df3f5f163f59d82338f565feb17cd6
Nemanja Dragović <nemanjadragovic94@gmail.com>
2017-08-07 23:52:29 +0200

    project_task_forecasts: apply milestone template action updated

--- project_task_forecasts/models/milestone_template.py
+++ project_task_forecasts/models/milestone_template.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import logging
 from openerp import models, fields, api, exceptions as ex
+
+_logger = logging.getLogger(__name__)
 
 
 class MilestoneTemplate(models.Model):
@@ -17,16 +20,21 @@ class MilestoneTemplate(models.Model):
     def default_project_template_constrains(self):
         if self.is_default is True and self.project_id:
             if self.search([('is_default', '=', True), ('project_id', '=', self.project_id.id), ('id', '!=', self.id)], count=True) > 0:
-                raise e.ValidationError('Only one default template can be in a project.')
+                raise ex.ValidationError('Only one default template can be in a project.')
 
     @api.one
     def update_milestones(self):
         err_msgs = []
-        for task in self.env['project.task'].search([('milestone_template_id', '=', self.id)]):
+        task_list = self.env['project.task'].search([('milestone_template_id', '=', self.id)])
+        task_counter = len(task_list)
+        _logger.info('Apply template action started for %s tasks...' % (task_counter))
+        for task in task_list:
             try:
                 task.apply_milestone_template()
+                task_counter -= 1
+                _logger.info('Template applied on task %s. Remaining %s.' % (task.name, task_counter))
             except Exception as e:
-                err_msgs.append(e.name)
+                err_msgs.append(str(e))
 
         if len(err_msgs) > 0:
             raise ex.UserError('\n\n\n'.join(err_msgs))
